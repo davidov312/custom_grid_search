ef29968b631b2cc04e71018f72e6c95344396b39
David <davidov312@gmail.com>
2022-01-03 09:20:44 +0200

    The class was rewritten to support saving results after each iteration. The saving is not implemented

--- custom_grid_search.py
+++ custom_grid_search.py
@@ -9,13 +9,14 @@ import numpy as np
 from sklearn.model_selection import KFold
 
 
- 
-class GridSearch():
-    def __init__(self, Model: object, params:dict, num_folds:int, metric):
+class CustomGridSearch():
+    def __init__(self, Model: object, params:dict, train_folds, metric):
         self.Model = Model
         self.params = params
         self.metric = metric
-        self.num_folds = num_folds
+        self.train_folds = train_folds #int or list
+        self.num_folds = train_folds if type(train_folds)==int else len(train_folds)
+        self._get_params_df()
 
 
     def _get_params_df(self):
@@ -33,19 +34,24 @@ class GridSearch():
         return self.metric(y_val, y_pred)
 
 
-    def fit(self, X, y):
-        self._get_params_df()
-        df = pd.DataFrame()
-        kfold = KFold(n_splits=self.num_folds)
-        for i,(tr_idx, val_idx) in enumerate(kfold.split(X)):
-            df[f'result_{i}'] = self.param_df.apply(lambda row: self._get_results_trial(row, X[tr_idx], y[tr_idx], X[val_idx], y[val_idx]), axis=1)
-        self.df = pd.concat([self.param_df, df], axis=1)
-    
-    
     def get_best_params(self):
         self.df['result'] = self.df[[f'result_{i}' for i in range(self.num_folds)]].mean(axis=1)
         self.df = self.df.drop([f'result_{i}' for i in range(self.num_folds)], axis=1)
         return self.df[self.df.result == self.df.result.max()].iloc[0].drop('result').to_dict() # iloc is for the case of multiple parameters with the highest result
+    
+    
+    def fit(self, X, y):
+        df = self.param_df.copy()
+        df[[f'result_{i}' for i in range(self.num_folds)]] = None
+        kfold = KFold(n_splits=self.num_folds).split(X) if type(self.train_folds) == int else self.train_folds
+        
+        for i in range(len(df)):
+            for j,(tr_idx, val_idx) in enumerate(kfold):
+                if df.loc[i, [f'result_{j}']].isnull().values:
+                    df.loc[i, [f'result_{j}']] = self._get_results_trial(self.param_df.loc[i], X[tr_idx], y[tr_idx], X[val_idx], y[val_idx])
+    
+        self.df = df
+        self.best_params_ = self.get_best_params()
     
 
 
